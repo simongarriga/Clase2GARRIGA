291daf87002b41efc9fb00c6fd7e3e0eb3071826
simongarriga <83841211+simongarriga@users.noreply.github.com>
2021-11-03 16:07:10 -0300

    Add files via upload

---
+++ Clase2GARRIGA/app2.js
@@ -0,0 +1,37 @@
+//EJEMPLO 1
+
+// let numeroX = 35;
+
+// if (numeroX < 35) {
+//     console.log('El numero es menor');
+// } else if (numeroX > 35) {
+//     console.log('El numero es mayor');
+// } else (numeroX == 35)
+
+//EJEMPLO 2
+
+// let efectivo = 300;
+// let credito = 400;
+// let disponible = efectivo + credito;
+// let totalCarrito = 700;
+
+// if(efectivo > totalCarrito || credito > totalCarrito ) {
+//     console.log('Puedes pagar con efectivo o credito');
+// } else if(disponible >= totalCarrito) {
+//     console.log('Paga con ambos');
+// } else {
+//     console.log('No podes pagar');
+// }
+
+//EJEMPLO 3
+
+let numeroX = parseInt(prompt('Decime tu numero favorito!'));
+
+if (numeroX >= 10 && numeroX <= 50) {
+    alert('Tu numero es ganador del premio CoderHouse!');
+} else {
+    alert('Tu numero es incorrecto, segui participando!');
+}
+
+
+
